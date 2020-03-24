f4b471cd2ae94baa819922e481fdad9d70960b91
Evan Gubarev <evan@evangubarev.com>
2020-03-23 21:32:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyblocktext',
-    version='0.1',
+    version='1.1',
     packages=find_packages(exclude=['.github*']),
     license='MIT',
     description='a python package to generate blocktext',
